9b6fb0641c20c9514bc52146b2ae779c09ebf50b
Thomas Hooge <privat@hoogi.de>
2016-08-31 10:22:17 +0200

    Initiale Version der Konfiguration für die Stable-Version

---
+++ site.mk
@@ -0,0 +1,35 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-luci-private-wifi \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+DEFAULT_GLUON_RELEASE := 0.7.1
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+
+GLUON_LANGS ?= en de
